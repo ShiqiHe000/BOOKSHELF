05ec18c1b5f5fd4b3c1480720db8b27f23bf8077
Shiqi He <she010@uottawa.ca>
2021-05-23 13:47:53 -0400

    some css

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-const methodOverride = require('method-override');
+const methodOverride = require("method-override");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ limit: "10mb", extended: false }));
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "./layouts/layout");
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 app.use(expressLayouts);
 app.use(express.static("public"));
 
